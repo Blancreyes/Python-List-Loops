422c8910557cbd567ac66d7b02e637f64ea1117b
Blancreyes <luijbr83@gmail.com>
2023-02-20 08:51:01 +0000

    Third commit

--- exercises/08.1-Merge_list/app.py
+++ exercises/08.1-Merge_list/app.py
@@ -4,5 +4,14 @@ chunk_two = [ 'Lucas' , 'Jake','Scott','Amy', 'Molly','Hannah','Lucas']
 
 def merge_list(list1, list2):
     #Your code go here:
-    
-print(merge_list(chunk_one, chunk_two))
+    merged_list=[]
+    for x in list1:
+        merged_list.append(x)
+        print(merged_list)
+    for y in list2:
+        merged_list.append(y)  
+    return merged_list
+
+# print(merge_list(chunk_one, chunk_two))
+print (merge_list(chunk_one, chunk_two))
+
